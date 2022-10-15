168c27bd0391ee2ce2ee3892b5cf438631fe2149
herbheebhart <abibatsalaudeen71@gmail.com>
2022-10-15 17:14:37 +0100

    donate

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const countDown = ()=>{
     const hrs = mins * 60
     const days = hrs * 24
     // calculate the rubbish
-    const textDay =Math.floor(gap/days)
+    const textSpace =Math.floor(gap/days)
     const textHour = Math.floor((gap % days)/hrs)
     const textMin = Math.floor( (gap  % hrs)/mins)
     const textSec = Math.floor( (gap % mins)/secs)
 
 
     // Dom
-    document.querySelector('.day').innerHTML = textDay
+    document.querySelector('.day').innerHTML =textSpace;
     document.querySelector('.hour').innerHTML =formatTime(textHour)
     document.querySelector('.min').innerHTML =formatTime(textMin)
     document.querySelector('.sec').innerHTML =formatTime(textSec)
@@ -40,26 +40,39 @@ faq.forEach((faqs)=>{
         }
     })
 })
-// payment method is paypal
-var amountElement = document.getElementById('amount');
-
-paypal.Buttons({
-    createOrder: function(data, actions) {
-      // This function sets up the details of the transaction, including the amount and line item details.
-      return actions.order.create({
-        purchase_units: [{
-          amount: {
-            value: amountElement.value
-          }
-        }]
-      });
-    },
-    onApprove: function(data, actions) {
-      // This function captures the funds from the transaction.
-      return actions.order.capture().then(function(details) {
-        // This function shows a transaction success message to your buyer.
-        alert('Transaction completed by ' + details.payer.name.given_name);
-      });
-    }
-  }).render('#paypal');
   //This function displays payment buttons on your web page.
+  // payStack
+  const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_34c83ef35135f7afb77dbe3b40d8b0a6803b6a34', // Replace with your public key
+    email: document.getElementById("email-address").value,
+    contact:document.getElementById("contact").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
+}
+// popup for
+var formPop = document.getElementById('myForm');
+var openModal = document.getElementById('openModal');
+var closeModal = document.getElementById('closeModal');
+
+openModal.onclick = function(){
+  formPop.style.display = "block"
+}
+closeModal.onclick = function(){
+  formPop.style.display = "none"
+}
